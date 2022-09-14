075e24acb64c2f697796b315fd6218194c3bcd0b
RobloxDevs <78046604+RobloxDevs@users.noreply.github.com>
2022-09-15 05:47:48 +0900

    Rename window.js to window.user.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const b = typeof browser !== 'undefined' ? browser : chrome;
 
 const script = document.createElement('script');
-script.src = b.runtime.getURL('window.js');
+script.src = b.runtime.getURL('window.user.js');
 document.documentElement.appendChild(script);
