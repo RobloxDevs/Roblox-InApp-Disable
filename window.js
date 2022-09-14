6bcbc1cdd7e033f290deba254550effee636a63f
RobloxDevs <78046604+RobloxDevs@users.noreply.github.com>
2022-09-15 05:44:28 +0900

    Support tampermonkey

--- window.js
+++ window.js
@@ -1,7 +1,21 @@
-window.navigator.msLaunchUri = (url) => {
-    const iframe = document.createElement('iframe');
-    iframe.id = 'gamelaunch';
-    iframe.className = 'hidden';
-    iframe.src = url.replace('InApp', 'InBrowser');
-    document.getElementsByTagName('body')[0].append(iframe);
-}; // Hack IE method
+// ==UserScript==
+// @name         Roblox-InApp-Disable
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Disable InApp for roblox
+// @author       RobloxDevs
+// @match        https://*.roblox.com/games/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.navigator.msLaunchUri = (url) => {
+        const iframe = document.createElement('iframe');
+        iframe.id = 'gamelaunch';
+        iframe.className = 'hidden';
+        iframe.src = url.replace('InApp', 'InBrowser');
+        document.getElementsByTagName('body')[0].append(iframe);
+    }; // Hack IE method
+})();
